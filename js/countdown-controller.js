ce411cce76be4a99b30e6582f627293e512dcc81
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2024-12-09 17:29:08 +0800

    fix countdown and add ngunduh mantu

--- js/countdown-controller.js
+++ js/countdown-controller.js
@@ -1,13 +1,34 @@
-simplyCountdown(".simply-countdown", {
-	year: 2024, // required
-	month: 12, // required
-	day: 7, // required
-	hours: 7, // Default is 0 [0-23] integer
-	words: {
-		//words displayed into the countdown
-		days: { singular: "hari", plural: "hari" },
-		hours: { singular: "jam", plural: "jam" },
-		minutes: { singular: "menit", plural: "menit" },
-		seconds: { singular: "detik", plural: "detik" },
-	},
-});
+const dDayDate = new Date("2024-12-07");
+const currentDate = new Date();
+const nextDayAfterDday = new Date(dDayDate);
+nextDayAfterDday.setDate(dDayDate.getDate() + 1);
+
+const messageElement = document.querySelector(".congrats-message");
+
+if (currentDate >= nextDayAfterDday) {
+  messageElement.innerText = "Selamat Menempuh Hidup Baru";
+  document.querySelector(".simply-countdown").style.display = "none";
+} else {
+  simplyCountdown(".simply-countdown", {
+    year: dDayDate.getFullYear(), // required
+    month: dDayDate.getMonth() + 1, // required
+    day: dDayDate.getDate(), // required
+    hours: 7, // Default is 0 [0-23] integer
+    words: {
+      //words displayed into the countdown
+      days: { singular: "hari", plural: "hari" },
+      hours: { singular: "jam", plural: "jam" },
+      minutes: { singular: "menit", plural: "menit" },
+      seconds: { singular: "detik", plural: "detik" },
+    },
+    onEnd: function () {
+      // Change the message on D-Day
+      messageElement.innerText = "Wedding Day";
+      // Optionally hide the countdown
+      document.querySelector(".simply-countdown").style.display = "none";
+    },
+  });
+
+  // Set the initial message
+  messageElement.innerText = "";
+}
